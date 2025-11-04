6924be03a51f3bd0f29fb8c107ba340aa1d8ff5d
Tomac Barin Jansson <tomacbarin@me.com>
2025-11-04 11:20:15 +0100

    Same version, just on repeat.

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// Variabler
+// Hämta knapp
+const btnHtml = document.querySelector("#btn");
+const paraHtml = document.querySelector("#displayResult");
 
-// → Hämta väderdata från Open-Meteo
+function fetchWeather() {
+  fetch("https://api.open-meteo.com/v1/forecast?latitude=56.05&longitude=12.70&current_weather=true")
+    .then(response => response.json())
+    .then(data => {
+      const temperature = data.current_weather.temperature;
+      paraHtml.textContent = `Temperaturen i Helsingborg är ${temperature}°C`;
+    })
+    .catch(error => {
+        paraHtml.textContent = "Kunde inte hämta data.";
+        console.error("Fel vid hämnting av väder", error);
+    })
+}
 
-// → Ett Promise
-
-// → När .then() körs
-
-// → a) Kolla om svaret är ok
-// → b) Gör om till JSON
-// → c) Plocka ut temperaturen
-// → d) Skriv ut i konsolen
+btnHtml.addEventListener("click", () => {
+    fetchWeather()
+    
+});
