6e6ab3e4e9d76411c1e833f2ed9af3f839ae84a5
Tomac Barin Jansson <tomacbarin@me.com>
2025-11-04 08:36:55 +0100

    The basics. All set.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Variabler
+
+// → Hämta väderdata från Open-Meteo
+
+// → Ett Promise
+
+// → När .then() körs
+
+// → a) Kolla om svaret är ok
+// → b) Gör om till JSON
+// → c) Plocka ut temperaturen
+// → d) Skriv ut i konsolen
